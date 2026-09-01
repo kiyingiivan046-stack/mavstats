d167c892456f25902d99c30ad932c3713ae049bc
kiyingiivan046-stack <kiyingiivan046@gmail.com>
2026-09-01 15:26:12 +0300

    Add initial script for MaveriQuant Analytics

---
+++ script.js
@@ -0,0 +1,8 @@
+// MaveriQuant Analytics
+// Website JavaScript
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("MaveriQuant Analytics website loaded successfully.");
+
+});
