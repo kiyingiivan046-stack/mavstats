21e31ff68a1be98e94f6c6435b7454830e7b487a
Ponsiano <123456789+kiyingiivan046-stack@users.noreply.github.com>
2026-09-04 22:17:49 +0300

    my second push

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("MaveriQuant Analytics website loaded successfully.");
 
 });
+fetch("header.html")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Could not load header.html");
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById("header-placeholder").innerHTML = data;
+    })
+    .catch(error => {
+        console.error("Header loading error:", error);
+    });
